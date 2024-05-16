fa41c7be3410af9cc27b2c9ed6612d3c299581ed
devopsjaba <devopsjaba@gmail.com>
2024-05-16 20:23:35 +0900

    Week02

---
+++ eclipse-workspace/OperatorTest/src/kr/co/kosta/Operator2.java
@@ -0,0 +1,12 @@
+package kr.co.kosta;
+
+public class Operator2 {
+	public static void main(String[] args) {
+		System.out.println(2 + 3);
+		System.out.println(8 - 5);
+		System.out.println(7 * 2);
+		System.out.println(7 / 2);
+		System.out.println(8 % 5);
+	}
+	
+}
---
+++ eclipse-workspace/OperatorTest/src/kr/co/kosta2/Operator.java
@@ -0,0 +1,15 @@
+package kr.co.kosta2;
+
+// 증가,증감 연산자
+public class Operator {
+	public static void main(String[] args) {
+		int a = 3;
+		++a;		// a값에 1을 더하라는 명령
+		System.out.println(a); 		//4
+		
+		int b = 3;
+		b++;		// b값에 1을 더하라는 명령
+		System.out.println(b); 		//4
+		
+	}
+}
