d3b6842aafea0ac22b50d3b0d03b368bfe150337
devopsjaba <devopsjaba@gmail.com>
2024-07-17 19:44:24 +0900

    Week11

--- workspace-db/query9.sql
+++ workspace-db/query9.sql
@@ -7,7 +7,8 @@ create table users (
     useremail	varchar(50)		not null
 );
 
-drop table boards (
+drop table boards;
+create table boards (
 	bid			int				primary key	auto_increment,
     btitle		varchar(100)	not null,
     bcontent	longtext		not null,
@@ -16,6 +17,35 @@ drop table boards (
     bfilename	varchar(50)		null,
     bfiledata	longblob		null
 );
+
+drop table accounts;
+create table accounts (
+	aid			varchar(20)		primary key,
+    owner		varchar(20)		not null,
+    balance		numeric			not null
+);
+
+
+INSERT INTO jdbc.users (userid, username, userpassword, userage, useremail) 
+VALUES('kosta1', '코스타1', 'pass123#', 25, 'kosta1@kosta.com');
+
+
+INSERT INTO jdbc.boards
+(btitle, bcontent, bwriter, bdate, bfilename)
+VALUES('비오는 수요일', '장마가 이제 시작되었습니다.', 'kosta1', now(), 'event.png');
+
+
+UPDATE jdbc.boards
+SET btitle='비오는 수요일', bcontent='장마가 이제 시작되었습니다', bwriter='kosta1', bfilename='event2.png'
+WHERE bid=1;
+
+DELETE FROM jdbc.boards
+WHERE bid=1;
+
+DELETE FROM jdbc.boards WHERE bwriter='kosta1';
+
+
+
 
 
 
