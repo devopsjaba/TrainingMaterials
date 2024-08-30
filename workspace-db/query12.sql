04b5e4db71a6a2a36370b9640aa8b342e09d4b55
devopsjaba <devopsjaba@gmail.com>
2024-08-30 19:30:01 +0900

    Week17

--- workspace-db/query12.sql
+++ workspace-db/query12.sql
@@ -23,8 +23,42 @@ SELECT bno, title, content, writer, view_cnt, comment_cnt, reg_date
 FROM board
 where bno = 1;
 
+INSERT INTO springbasic.board
+(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+VALUES('8월 마지막 주', '태풍10호가 북상중입니다.', 'kosta', 0, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+INSERT INTO springbasic.board
+(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+VALUES('8월 마지막 주 수요일', '태풍10호가 북상중입니다. 언제올라오나요?', 'ori', 0, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+INSERT INTO board
+(title, content, writer)
+VALUES('스프링 게시판', '게시판 구현중입니다', 'kosta');
 
+delete from board where bno = 2 and writer = 'kosta';
 
+UPDATE board
+SET title='8월 마지막 주', content='태풍10호가 북상중입니다.', up_date= now()
+WHERE bno=4 and writer = 'kosta';
 
+select count(*) from board;
+
+SELECT bno, title, content, writer, view_cnt, comment_cnt, reg_date
+FROM board
+order by reg_date desc, bno desc 
+limit 10 offset 0; 
+
+update board
+set view_cnt = view_cnt + 1
+where bno = 586;
+
+select * from board
+where true and title like 'Pioneering1%'; 
+
+select * from board
+where true and title like concat('Pioneering1', '%'); 
+
+SELECT bno, title, content, writer, view_cnt, comment_cnt, reg_date
+	FROM board
+where bno IN (337, 338, 339)
+order by reg_date desc, bno desc;    
