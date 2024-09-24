9ce5293922f081e842eeb79342526c9dab80b6f1
devopsjaba <devopsjaba@gmail.com>
2024-09-24 20:53:52 +0900

    Week21

---
+++ React-workspace/kmall/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-4xl font-extrabold m-2 p-3 bg-green-500'>
+      Hello 월드!
+    </div>
+  );
+}
+
+export default App;
