7359c0c7f9664c6d7a06b0a8b329301adccb47be
devopsjaba <devopsjaba@gmail.com>
2024-09-24 20:07:19 +0900

    Week21

---
+++ sboot-intelij-workspace/hboot/src/main/java/com/kosta/hboot/HBootApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.hboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HBootApplication.class, args);
+	}
+
+}
---
+++ sboot-intelij-workspace/hboot/src/test/java/com/kosta/hboot/HBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.hboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
