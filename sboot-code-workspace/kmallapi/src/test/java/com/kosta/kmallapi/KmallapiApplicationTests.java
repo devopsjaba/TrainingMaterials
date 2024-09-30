0bd52d860347b4ed98b06d7d0adf00dd7986fefe
devopsjaba <devopsjaba@gmail.com>
2024-09-30 14:20:55 +0900

    Week22

---
+++ sboot-code-workspace/kmallapi/src/test/java/com/kosta/kmallapi/KmallapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.kmallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
